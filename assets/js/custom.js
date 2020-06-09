d3628d9ecf78e960d32717d6df2346abf6adeb4f
John Griffiths <gnuchu@gmail.com>
2020-06-09 21:20:52 +0100

    Bootstrap starter

---
+++ assets/js/custom.js
@@ -0,0 +1,11 @@
+$(function(){
+  setInterval(localSetTime, 5000);
+})
+
+function getSite() {
+  //https://jsonplaceholder.typicode.com/todos/1
+}
+
+function localSetTime() {
+  $('#content').html("John");
+}
